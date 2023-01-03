f8fd11f1b20103af6ece76701dfce8cf3e407b0a
Mike Zhang <mikz@itu.dk>
2023-01-03 16:02:53 +0100

    Update run.finetune.sh

--- run.finetune.sh
+++ run.finetune.sh
@@ -9,8 +9,9 @@ PARAMETERS=$2
 for c in 1 2 3 4 5
 do
 
+mkdir logs
 echo "Training $MODEL on $PARAMETERS"
-python3 train.py --dataset_config configs/$PARAMETERS.json --parameters_config configs/$MODEL/$MODEL.$c.json --device 0 --name esco.new.$MODEL.$PARAMETERS.$c
+python3 train.py --dataset_config configs/$PARAMETERS.json --parameters_config configs/$MODEL/$MODEL.$c.json --device 0 --name logs/esco.new.$MODEL.$PARAMETERS.$c
 
 done
 
